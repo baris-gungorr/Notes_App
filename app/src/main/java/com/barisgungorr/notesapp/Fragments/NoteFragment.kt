3fb1faeea2d39228ff49990ef0a7dae96b0cfbea
Baris Gungor <gungorbaris41@gmail.com>
2023-07-24 16:35:51 +0300

    add note adapter and note fragment created

--- app/src/main/java/com/barisgungorr/notesapp/Fragments/NoteFragment.kt
+++ app/src/main/java/com/barisgungorr/notesapp/Fragments/NoteFragment.kt
@@ -5,56 +5,68 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.barisgungorr.Models.NoteModel
+import com.barisgungorr.notesapp.Adapters.NoteAdapter
 import com.barisgungorr.notesapp.R
+import com.google.android.material.transition.MaterialElevationScale
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NoteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NoteFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var addNoteFab: LinearLayout
+
+    lateinit var noteUser: ImageView
+
+    lateinit var search: EditText
+
+    lateinit var chatFabText: TextView
+
+    private var backPressedTime: Long = 0
+
+    private var backToast: Toast? = null
+
+    lateinit var rvNote: RecyclerView
+
+    lateinit var noData: ImageView
+
+    lateinit var options: FirestoreRecyclerOptions<NoteModel>
+
+    lateinit var firebaseAdapter: NoteAdapter
+
+    lateinit var noteGrid: ImageView
+
+    private var isGrid=false
+
+
+
+    var layoutManager =
+        StaggeredGridLayoutManager(1
+            , StaggeredGridLayoutManager.VERTICAL)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+        exitTransition = MaterialElevationScale(false).apply {
+            duration = 350
         }
+        enterTransition = MaterialElevationScale(true).apply {
+            duration = 350
+        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_note, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NoteFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NoteFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
