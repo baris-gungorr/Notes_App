64c6ab3c4d3fb6f6909f5d390134e71f70ec4ef5
Baris Gungor <gungorbaris41@gmail.com>
2023-07-27 23:51:50 +0300

    Add sign_in codes

--- app/src/main/java/com/barisgungorr/notesapp/Fragments/Sign_in_Fragment.kt
+++ app/src/main/java/com/barisgungorr/notesapp/Fragments/Sign_in_Fragment.kt
@@ -1,20 +1,25 @@
 package com.barisgungorr.notesapp.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat.startActivity
+import androidx.navigation.Navigation
+import com.barisgungorr.notesapp.Activity.NoteActivity
 import com.barisgungorr.notesapp.R
+import com.barisgungorr.notesapp.databinding.FragmentSignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class Sign_in_Fragment : Fragment() {
+    private lateinit var binding: FragmentSignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,4 +29,64 @@ class Sign_in_Fragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sign_in_, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentSignInBinding.bind(view)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.goToSignUp.setOnClickListener {
+
+            Navigation.findNavController(view).navigate(R.id.) // kodla
+        }
+
+        binding.goToForgotPassword.setOnClickListener {
+
+            Navigation.findNavController()  // kodla
+        }
+
+        binding.login.setOnClickListener {
+            val mail: String = binding.loginemail.toString().trim()
+            val password: String = binding.loginpassword.text.toString().trim()
+            if (mail.isEmpty() || password.isEmpty())
+                Toast.makeText(requireContext(), "Alanları doldurunuz!", Toast.LENGTH_LONG).show()
+
+
+
+
+            binding.progressBar.visibility = View.VISIBLE
+
+            firebaseAuth.signInWithEmailAndPassword(mail, password).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+
+                    checkMailVerification()
+                } else {
+                    Toast.makeText(requireContext(), "Hesap Mevcut Değil!", Toast.LENGTH_LONG)
+                        .show()
+                    binding.progressBar.visibility = View.INVISIBLE
+                }
+            }
+        }
+
+    }
+
+    private fun checkMailVerification () {
+
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser!!.isEmailVerified) {
+            Toast.makeText(requireContext(),"Başarılı Giriş",Toast.LENGTH_SHORT).show()
+            binding.progressBar.visibility = View.INVISIBLE
+
+            val intent =Intent(activity,NoteActivity::class.java)
+            startActivity(intent)
+
+            activity?.finish()
+
+        } else {
+            Toast.makeText(requireContext(),"E-mail adresini doğrulayın",Toast.LENGTH_SHORT).show()
+            binding.progressBar.visibility = View.INVISIBLE
+            firebaseAuth.signOut()
+        }
+
+    }
 }
