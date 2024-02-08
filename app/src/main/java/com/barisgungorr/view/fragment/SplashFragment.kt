2936c42100efac4c28f841dbc7fe4d64250076ed
Baris Gungor <gungorbaris41@gmail.com>
2024-02-08 15:46:43 +0300

    Update setupOnClickListeners in SplashFragment: Moved the isCheckboxChecked condition inside the click listeners to ensure its checked every time a click event occurs. This change is necessary to capture the updated state of the checkbox whenever a click event happens.

--- app/src/main/java/com/barisgungorr/view/fragment/SplashFragment.kt
+++ app/src/main/java/com/barisgungorr/view/fragment/SplashFragment.kt
@@ -1,5 +1,6 @@
 package com.barisgungorr.view.fragment
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -7,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.barisgungorr.data.InformationModel
@@ -14,13 +16,13 @@ import com.barisgungorr.notesapp.R
 import com.barisgungorr.notesapp.databinding.FragmentSplashBinding
 import com.barisgungorr.view.activity.NoteActivity
 import com.barisgungorr.view.utils.Constants
-import com.barisgungorr.view.utils.Constants.RC_SIGN_IN
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.database.FirebaseDatabase
@@ -29,7 +31,6 @@ class SplashFragment : Fragment() {
 
     private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
-
     private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var firebaseAuth: FirebaseAuth
 
@@ -46,134 +47,159 @@ class SplashFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         firebaseAuth = FirebaseAuth.getInstance()
-
         checkUser()
+        setupGoogleSignIn()
+        setupOnClickListeners()
 
+    }
+
+    private val signInResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                try {
+                    val account = task.getResult(ApiException::class.java)
+                    firebaseAuthWithGoogleAccount(account)
+                } catch (e: ApiException) {
+                    Toast.makeText(
+                        requireContext(),
+                        R.string.splash_fragment_sign_in_failed,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+
+
+    private fun setupGoogleSignIn() {
         val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(Constants.CLIENT_ID_SP)
             .requestEmail()
             .build()
 
         googleSignInClient = GoogleSignIn.getClient(requireActivity(), googleSignInOptions)
+    }
 
-        binding.termsConditions.setOnClickListener {
-            val openUrlIntent = Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse("https://gungorbarisdevelop.blogspot.com/2023/07/uygulamasi-kosullar-ve-sartlar.html")
-            )
-            startActivity(openUrlIntent)
-        }
+    private fun setupOnClickListeners() {
+        val isCheckboxChecked = binding.checkbox.isChecked
+
+        binding.termsConditions.setOnClickListener { openTermsAndConditionsUrl() }
 
         binding.googleSignIn.setOnClickListener {
-            if (binding.checkbox.isChecked) {
-                val intent = googleSignInClient.signInIntent
-                startActivityForResult(intent, RC_SIGN_IN)
+            if (isCheckboxChecked) {
+                val signInIntent = googleSignInClient.signInIntent
+                signInResultLauncher.launch(signInIntent)
             } else {
-                Toast.makeText(
-                    requireContext(),
-                    "Şartlar & Koşullar kabul edilmelidir!",
-                    Toast.LENGTH_SHORT
-                ).show()
+                showTermsAndConditionsToast()
             }
         }
 
         binding.signIn.setOnClickListener {
-            if (binding.checkbox.isChecked) {
+            if (isCheckboxChecked) {
                 findNavController().navigate(R.id.action_splash_Fragment_to_sign_in_Fragment)
             } else {
-                Toast.makeText(
-                    requireContext(),
-                    R.string.splash_fragment_term_conditions,
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        when (requestCode) {
-
-            RC_SIGN_IN -> {
-                val accountTask = GoogleSignIn.getSignedInAccountFromIntent(data)
-                try {
-                    val account = accountTask.getResult(ApiException::class.java)
-                    firebaseAuthWithGoogleAccount(account)
-                } catch (e: Exception) {
-                    Toast.makeText(requireContext(), "Başarısız kayıt", Toast.LENGTH_SHORT).show()
-                }
+                showTermsAndConditionsToast()
             }
         }
     }
 
     private fun firebaseAuthWithGoogleAccount(account: GoogleSignInAccount?) {
-        val credential = GoogleAuthProvider.getCredential(account!!.idToken, null)
-        firebaseAuth.signInWithCredential(credential)
-            .addOnSuccessListener {
+        if (account != null) {
+            val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+            firebaseAuth.signInWithCredential(credential)
+                .addOnSuccessListener { handleSignInSuccess(it) }
+                .addOnFailureListener { handleSignInFailure(it) }
+        } else {
+            Toast.makeText(
+                requireContext(),
+                R.string.splash_fragment_such_account,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
 
-                val firebaseUser = firebaseAuth.currentUser
+    private fun handleSignInSuccess(result: AuthResult) {
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null) {
+            if (result.additionalUserInfo!!.isNewUser) {
+                val user = InformationModel(
+                    firebaseUser.displayName.toString(),
+                    firebaseUser.email.toString(),
+                    "",
+                    firebaseUser.photoUrl.toString(),
+                    firebaseUser.uid.toString()
+                )
 
-                if (it.additionalUserInfo!!.isNewUser) {
-                    val user = InformationModel(
-                        firebaseUser?.displayName.toString(),
-                        firebaseUser?.email.toString(),
-                        "",
-                        firebaseUser?.photoUrl.toString(),
-                        firebaseUser?.uid.toString()
-                    )
+                FirebaseDatabase.getInstance().reference.child("Users")
+                    .child(firebaseUser.uid)
+                    .setValue(user)
 
-                    if (firebaseUser != null) {
-                        FirebaseDatabase.getInstance().reference.child("Users")
-                            .child(firebaseUser.uid)
-                            .setValue(user)
-
-                        Toast.makeText(
-                            requireContext(),
-                            "Başarılı kayıt",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    } else {
-                        Toast.makeText(
-                            requireContext(),
-                            "Kullanıcı bulunamadı!",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                } else {
-                    Toast.makeText(requireContext(), "Succes", Toast.LENGTH_SHORT).show()
-                }
-
-                requireActivity().startActivity(Intent(requireContext(), NoteActivity::class.java))
-
-                requireActivity().overridePendingTransition(0, 0)
-
-                requireActivity().finish()
-
+                Toast.makeText(
+                    requireContext(),
+                    R.string.splash_fragment_Successful_registration,
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    R.string.splash_fragment_Successful_registration,
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-            .addOnFailureListener {
-                Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
-            }
+
+            startNoteActivity()
+        } else {
+            Toast.makeText(
+                requireContext(),
+                R.string.splash_fragment_user_notFound,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun handleSignInFailure(exception: Exception) {
+        Toast.makeText(requireContext(), exception.message, Toast.LENGTH_SHORT).show()
     }
 
     private fun checkUser() {
         val firebaseUser = firebaseAuth.currentUser
 
         if (firebaseUser != null && firebaseUser.isEmailVerified) {
-            startActivity(Intent(requireActivity(), NoteActivity::class.java))
-            activity?.overridePendingTransition(0, 0)
-            activity?.finish()
+            startNoteActivity()
         } else {
-            try {
-                Auth.GoogleSignInApi.signOut(googleSignInClient.asGoogleApiClient())
-            } catch (e: Exception) {
-                println(e.printStackTrace())
-            }
-            firebaseAuth.signOut()
+            signOut()
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun signOut() {
+        try {
+            Auth.GoogleSignInApi.signOut(googleSignInClient.asGoogleApiClient())
+        } catch (e: Exception) {
+            println(e.printStackTrace())
+        }
+        firebaseAuth.signOut()
     }
+
+    private fun startNoteActivity() {
+        startActivity(Intent(requireActivity(), NoteActivity::class.java))
+        activity?.overridePendingTransition(0, 0)
+        activity?.finish()
+    }
+
+    private fun openTermsAndConditionsUrl() {
+        val openUrlIntent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse(Constants.TERMS_CONDITION)
+        )
+        startActivity(openUrlIntent)
+    }
+
+    private fun showTermsAndConditionsToast() {
+        Toast.makeText(
+            requireContext(),
+            R.string.splash_fragment_term_conditions,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
 }
--- app/src/main/java/com/barisgungorr/view/utils/Constants.kt
+++ app/src/main/java/com/barisgungorr/view/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.barisgungorr.view.utils
 
 object Constants {
     const val CLIENT_ID = "AIzaSyCoIZB0qQ3BbkYWw7KRL-A_kVboFKwciB4"
-    const val RC_SIGN_IN = 100
     const val CLIENT_ID_SP = "166513008091-0eeirmkqfk81hlkr2j590reid1sfjbff.apps.googleusercontent.com"
+    const val TERMS_CONDITION = "https://gungorbarisdevelop.blogspot.com/2023/07/uygulamasi-kosullar-ve-sartlar.html"
 }
